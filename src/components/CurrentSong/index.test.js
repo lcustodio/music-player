a3cc19ce5ffd0d4e765f8bd01befe93c8ecb3a77
Luis Custodio <luis.custodio@gmail.com>
2018-03-21 22:06:25 +0000

    feat(playqueue): add play queue and current song. (#1)
    * Add tests for both components and overall behaviour.
    * No connection with parent.

---
+++ src/components/CurrentSong/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CurrentSong from './index';
+
+describe.only('Current Song', () => {
+  it('should handle no song', () => {
+    const expected = shallow(<CurrentSong />);
+    expect(expected.html()).toContain('No music is playing');
+  });
+
+  it('should display artist name', () => {
+    const expected = shallow(<CurrentSong song={song()} />);
+    expect(expected.html()).toContain('Artist 1');
+  });
+});
+
+function song() {
+  return { id: 1, title: 'Title 1', artist: 'Artist 1', album: 'Album 1' };
+}
