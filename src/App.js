f4784855717345c64f2f10669f8fd6290eb6bb02
Luis Custodio <luis.custodio@gmail.com>
2018-03-22 22:52:59 +0100

    feat(currentsong): add content and style current song (#3)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import MusicPlayer from './components/MusicPlayer';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.onMusicSelected = this.onMusicSelected.bind(this);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const playQueue = Object.assign([], this.props.musics);
+    const currentSong = playQueue.shift();
+    this.setState({currentSong, playQueue});
+  }
+
+  onMusicSelected(songId) {
+   console.log('music selected');
+  } 
+
   render() {
     return (
       <div className="App">
@@ -13,9 +31,9 @@ class App extends Component {
           <div className="App-title">Music Player</div>
         </header>
         <main className="App-container">
-          <CurrentSong />
-          <PlayQueue />
-          <MusicPlayer />
+          <CurrentSong song={this.state.currentSong}/>
+          <PlayQueue songs={this.state.playQueue} onMusicSelected={this.onMusicSelected}/>
+          <MusicPlayer song={this.state.currentSong}/>
         </main>
       </div>
     );
--- src/components/PlayQueue/index.js
+++ src/components/PlayQueue/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 const PlayQueue = props => {
   if (!props.songs || props.songs.length === 0) {
-    return ( 
-      <div className="PlayQueue">
-        No musics to be player - sad
-    </div>);
+    return <div className="PlayQueue">No musics to be player - sad</div>;
   }
 
-  return props.songs.map(song => (
-    <div className="PlayQueue" onClick={() => props.onMusicSelected(song.id)}>
-      <div>{song.title}</div>
-      <div>{song.artist}</div>
-      <div>{song.album}</div>
+  return (
+    <div className="PlayQueue">
+      {props.songs.map(song => (
+        <div className="PlayQueue-container"key={song.id} onClick={() => props.onMusicSelected(song.id)}>
+          <div className="PlayQueue-musicTitle">{song.title}</div>
+          <div>{song.artist}</div>
+          <div>{song.album}</div>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
 export default PlayQueue;
